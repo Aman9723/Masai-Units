27e0d7c87214501ce81ceba1d78fd397ab3abc01
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-11-23 22:55:34 +0530

    added refresh and blacklist

--- NEM201/jwt/index.js
+++ NEM201/jwt/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const UserModel = require('./models/user.model');
 const jwt = require('jsonwebtoken');
 
+const blackList = [];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -25,11 +27,55 @@ app.post('/login', async (req, res) => {
                 name: user.name,
                 age: user.age,
             },
-            'SECRET1234'
+            'SECRET1234',
+            {
+                expiresIn: '5 mins',
+            }
         );
-        return res.send({ message: 'Login Success', token });
+        // generate refresh token
+        const refreshToken = jwt.sign(
+            {
+                id: user._id,
+                name: user.name,
+                age: user.age,
+            },
+            'REFRESHSECRET',
+            {
+                expiresIn: '7 days',
+            }
+        );
+        return res.send({ message: 'Login Success', token, refreshToken });
     }
     return res.status(401).send('Invalid credentials');
+});
+
+app.post('/refresh', async (req, res) => {
+    const refreshToken = req.headers['authorization'];
+    if (!refreshToken) {
+        return res.status(401).send('unathorized');
+    }
+    try {
+        const verification = jwt.verify(refreshToken, 'REFRESHSECRET');
+        // 1. store info in primary token then retrieve
+        // 2. with the help of id, get data from db
+        if (verification) {
+            const newToken = jwt.sign(
+                {
+                    id: verification.id,
+                    name: verification.name,
+                    age: verification.age,
+                },
+                'SECRET1234',
+                {
+                    expiresIn: '5 mins',
+                }
+            );
+            return res.send({ token: newToken });
+        }
+    } catch (e) {
+        // refresh token is jwt.TokenExpiredError, redirect user to login on frontend
+        res.send('refresh token expired');
+    }
 });
 
 app.get('/user/:id', async (req, res) => {
@@ -38,6 +84,10 @@ app.get('/user/:id', async (req, res) => {
     if (!token) {
         return res.send('Unauthorized');
     }
+    // if token already expired
+    if (blackList.includes(token)) {
+        return res.send('token already expired');
+    }
     try {
         // gives error on wrong token
         const verification = jwt.verify(token, 'SECRET1234');
@@ -45,9 +95,19 @@ app.get('/user/:id', async (req, res) => {
             const user = await UserModel.findOne({ _id: id });
             return res.send(user);
         }
-    } catch {
+    } catch (e) {
+        // put already expired token in blacklist
+        if (e.message === 'jwt expired') {
+            blackList.push(token);
+        }
         return res.send('Invalid token');
     }
+});
+
+app.post('/logout', async (req, res) => {
+    const token = req.headers['authorization'];
+    blackList.push(token);
+    return res.send('User logged out successfully');
 });
 
 app.get('/', (req, res) => res.send('hello'));
